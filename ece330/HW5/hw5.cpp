11e2311a8799d047f146b428a2574246e1611dc0
Zane Kominek <zanekominek@gmail.com>
2021-04-03 08:01:14 -0600

    hw5

--- ece330/HW5/hw5.cpp
+++ ece330/HW5/hw5.cpp
@@ -1,6 +1,3 @@
-// Fig. 8.13: fig08_13.cpp
-// Selection sort with pass-by-reference. This program puts values into an 
-// array, sorts them into ascending order and prints the resulting array.
 #include <iostream> 
 #include <iomanip>
 using namespace std;
@@ -12,64 +9,35 @@ int main()
 {
    const int arraySize = 10;
    int a[ arraySize ] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
-
-   cout << "Data items in original order\n";
-
-   for ( int i = 0; i < arraySize; ++i )
-      cout << setw( 4 ) << a[ i ];
-
-   selectionSort( a, arraySize ); // sort the array
-
-   cout << "\nData items in ascending order\n";
-
-   for ( int j = 0; j < arraySize; ++j )
-      cout << setw( 4 ) << a[ j ];
-
-   cout << endl;
-} // end main
-
-// function to sort an array
-void selectionSort( int * const array, const int size )
-{
-   int smallest; // index of smallest element
-
-   // loop over size - 1 elements
-   for ( int i = 0; i < size - 1; ++i )
-   {
-      smallest = i; // first index of remaining array
-
-// Fig. 8.13: fig08_13.cpp
-// Selection sort with pass-by-reference. This program puts values into an 
-// array, sorts them into ascending order and prints the resulting array.
-#include <iostream> 
-#include <iomanip>
-using namespace std;
-
-void selectionSort( int * const, const int ); // prototype
-void swap( int * const, int * const ); // prototype
-
-int main()
-{
-   const int arraySize = 10;
-   int a[ arraySize ] = { 2, 6, 4, 8, 10, 12, 89, 68, 45, 37 };
-
-   cout << "Data items in original order\n";
+   float b[ arraySize ] = {2.1, 6.2, 4.3, 8.4, 10.5, 12.6, 89.9, 68.7, 45.8, 37.0 };
+   cout << "Data items in int original order\n";
 
    for ( int i = 0; i < arraySize; ++i )
       cout << setw( 4 ) << a[ i ];
 
    selectionSort( a, arraySize ); // sort the array
+   
 
-   cout << "\nData items in ascending order\n";
+   cout << "\nData items in integer array ascending order\n";
 
    for ( int j = 0; j < arraySize; ++j )
       cout << setw( 4 ) << a[ j ];
-
    cout << endl;
+   cout << "Data items in float original order\n";
+
+   for ( int i = 0; i < arraySize; ++i )
+      cout << setw( 4 ) << a[ i ];
+   selectionSort(b, arraySize);
+   cout << "\nData items in float array ascending order\n";
+   for ( int j = 0; j < arraySize; ++j )
+      cout << setw( 4 ) << a[ j ];
+   coud << endl;
+   
 } // end main
 
 // function to sort an array
-void selectionSort( int * const array, const int size )
+template <class T>
+void selectionSort( T * const array, const int size )
 {
    int smallest; // index of smallest element
 
@@ -90,24 +58,9 @@ void selectionSort( int * const array, const int size )
 
 // swap values at memory locations to which
 // element1Ptr and element2Ptr point
-void swap( int * const element1Ptr, int * const element2Ptr )
+void swap( T * const element1Ptr, T * const element2Ptr )
 {
    int hold = *element1Ptr;
    *element1Ptr = *element2Ptr;
    *element2Ptr = hold;
 } // end function swap
-
-/**************************************************************************
- * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- **************************************************************************/
